6662039c6425874023dc8f28dfaa80b2c2cbcbcc
Agnieszka Herod <metalvege@gmail.com>
2018-10-04 15:47:20 +0200

    firebase integration with Inventory: displaying books in Inventory panel

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,17 +1,36 @@
 import React from "react";
 import BookView from "./BookView";
+import {fbase} from "../firebase";
 
 export default class Inventory extends React.Component{
 
+
+    constructor(){
+        super();
+        this.state = {
+            books: []
+        }
+    }
+    componentDidMount(){
+        this.ref = fbase.syncState("bookstore/books", {
+            context: this,
+            state: "books"
+        });
+    }
+
+    componentWillUnmount(){
+        fbase.removeBinding(this.ref)
+    }
+
     render(){
 
-        // const bookListing = this.props.books.map((element, index )=> {
-        //     return  <BookView key = {index} book = {element} addToOrder = {this.props.addOrder}/>
-        // });
+        const bookListing = this.state.books.map((element, index )=> {
+            return  <BookView key = {index} book = {element} addToOrder = {this.props.addOrder}/>
+        });
         return (
             <div className="inventory">
                 <h1>Inventory</h1>
-                    {/*{bookListing}*/}
+                    {bookListing}
             </div>
         )
     }
